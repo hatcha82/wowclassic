c356171661a6f39988872f8c9fab3ddad069a665
Changbeom Ha <hatcha82@gmail.com>
2019-07-30 16:54:59 +0900

    ac

--- gnuboard5/extend/version.extend.php
+++ gnuboard5/extend/version.extend.php
@@ -1,6 +1,16 @@
 <?php
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
-define('G5_JS_VER',  '171222');
-define('G5_CSS_VER', '171222');
+
+function GUID()
+{
+    if (function_exists('com_create_guid') === true)
+    {
+        return trim(com_create_guid(), '{}');
+    }
+    return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
+}
+
+define('G5_JS_VER',  GUID());
+define('G5_CSS_VER', GUID());
 ?>
