e55807011ce19c05aa2ac12f18ae6c3175df8ef6
Changbeom Ha <hatcha82@gmail.com>
2019-08-02 13:05:11 +0900

    카테고리 아이콘 추가

--- gnuboard5/lib/wowfunc.lib.php
+++ gnuboard5/lib/wowfunc.lib.php
@@ -18,6 +18,22 @@ function get_icon_by_categoryName($name){
     case "주술사"   :	$url=  "https://wow.zamimg.com/images/wow/icons/tiny/class_shaman.gif";break;
     case "드루이드" :	$url=  "https://wow.zamimg.com/images/wow/icons/tiny/class_druid.gif";break;
     case "사냥꾼"   :	$url=  "https://wow.zamimg.com/images/wow/icons/tiny/class_hunter.gif";break;
+
+    case "가죽세공"	:   $url= "/img/WOW/profession/skinning.png";break;
+    case "기계공학"	:   $url=  "/img/WOW/profession/engineering.png";break;
+    case "대장기술" :	$url=  "/img/WOW/profession/blacksmithing.png";break;
+    case "마법부여" :   $url=  "/img/WOW/profession/enchanting.png";break;
+    case "재봉"     :	$url=  "/img/WOW/profession/tailoring.png";break;
+    case "연금술"   :	$url=  "/img/WOW/profession/alchemy.png";break;
+    case "무둘질"   :	$url=  "/img/WOW/profession/leatherworking.png";break;
+    case "약초"     :	$url=  "/img/WOW/profession/herbalism.png";break;
+    case "채광"   :	$url=  "/img/WOW/profession/mining.png";break;
+    case "낚시"   :	$url=  "/img/WOW/profession/fishing.png";break;
+    case "요리"   :	$url=  "/img/WOW/profession/cooking.png";break;
+
+
+    
+
     default:
        $url = "";
     } 
