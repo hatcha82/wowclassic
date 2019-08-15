4a9cee3befd892855f935efebc34f42ef99f92a2
Changbeom Ha <hatcha82@gmail.com>
2019-08-15 14:25:43 +0900

    change

--- gnuboard5/theme/basic/mobile/youtube.php
+++ gnuboard5/theme/basic/mobile/youtube.php
@@ -11,9 +11,9 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
             limit 30";
     
     $youtubeIdList = array();
-    $result = sql_query($sql);
-    for ($i=0; $row=sql_fetch_array($result); $i++) {
-        preg_match('#(\.be/|/embed/|/v/|/watch\?v=)([A-Za-z0-9_-]{5,11})#',  $row['wr_content'], $matches);
+    $youtubeResult = sql_query($sql);
+    for ($i=0; $row3=sql_fetch_array($youtubeResult); $i++) {
+        preg_match('#(\.be/|/embed/|/v/|/watch\?v=)([A-Za-z0-9_-]{5,11})#',  $row3['wr_content'], $matches);
 
         if(isset($matches[2]) && $matches[2] != ''){
             $YoutubeCode = $matches[2];
@@ -23,7 +23,7 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
     }
     $YoutubeCode = $youtubeIdList[array_rand($youtubeIdList)];
 ?>
-<div class="lt list_01" style="padding:0;margin-bottom:0">
+<div class="lt list_01" >
     <div class="bo_name">
         <div class="lt_title">   
         <img src="/img/youtube.png" style="width:25px;float:left;margin-top:13px"/> 
@@ -31,16 +31,17 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
         </div>
     </div>
     <?php if(isset($YoutubeCode) && $YoutubeCode != ''){?>
-<div style="width:100%;padding:0px;margin:0 auto;">
-    <div id="youtube_area" style="width:100%;border:1px solid #444">
-        <div style="position: relative; padding-bottom: 56.25%;">
-        <iframe style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="https://www.youtube.com/embed/<?php echo $YoutubeCode?>?autoplay=0&playsinline=1" frameborder="0" gesture="media" allow="autoplay;encrypted-media" allowfullscreen="allowfullscreen"></iframe>
+    <div style="width:100%;padding:0px;margin:0 auto;">
+        <div id="youtube_area" style="width:100%;border:1px solid #444">
+            <div style="position: relative; padding-bottom: 56.25%;">
+            <iframe style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="https://www.youtube.com/embed/<?php echo $YoutubeCode?>?autoplay=0&playsinline=1" frameborder="0" gesture="media" allow="autoplay;encrypted-media" allowfullscreen="allowfullscreen"></iframe>
+            </div>
+            <!-- <a target="_blank" href="https://www.youtube.com/watch?v=<coYw-eVU0Ks" ></a>        -->
         </div>
-        <!-- <a target="_blank" href="https://www.youtube.com/watch?v=<coYw-eVU0Ks" ></a>        -->
-    </div>
+    </div> 
+    <?php }?>
 </div> 
-<?php }?> 
-</div>    
+    
 
 
    
