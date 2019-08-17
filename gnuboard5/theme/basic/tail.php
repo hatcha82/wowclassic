3c75578a184b469bcdff7493de2fe166bf42ea53
Changbeom Ha <hatcha82@gmail.com>
2019-08-17 22:43:20 +0900

    iframe 변경

--- gnuboard5/theme/basic/tail.php
+++ gnuboard5/theme/basic/tail.php
@@ -55,7 +55,7 @@ if (G5_IS_MOBILE) {
         <div style="width:230px;margin:0 auto;">
             <div id="youtube_area" style="width:230px;margin-top:10px;margin-bottom:20px;border:1px solid #444">
                 <div style="position: relative; padding-bottom: 56.25%;">
-                <iframe style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="https://www.youtube.com/embed/<?php echo $YoutubeCode?>?autoplay=0&playsinline=1" frameborder="0" gesture="media" allow="autoplay;encrypted-media" allowfullscreen="allowfullscreen"></iframe>
+                <iframe style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="https://www.youtube.com/embed/<?php echo $YoutubeCode?>?autoplay=0&playsinline=1" frameborder="0"  allow="autoplay;encrypted-media" allowfullscreen="allowfullscreen"></iframe>
                 </div>
                 <!-- <a target="_blank" href="https://www.youtube.com/watch?v=<coYw-eVU0Ks" ></a>        -->
             </div>
